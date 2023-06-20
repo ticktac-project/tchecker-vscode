883016774b80d438637c0d5e82d8cf209f3aaf9c
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-06-20 10:04:18 +0200

    Updated auto completion tool

--- src/tools/autoCompletion.ts
+++ src/tools/autoCompletion.ts
@@ -2,17 +2,15 @@ import * as vscode from 'vscode';
 
 import { keywords } from '../constants';
 
+function completeItem(item: string) {
+	const itemToComplete = new vscode.CompletionItem(item);
+	itemToComplete.kind = vscode.CompletionItemKind.Keyword;
+	return itemToComplete;
+}
+
 export function keywordsCompletion() {
 	return vscode.languages.registerCompletionItemProvider('tchecker', {
-
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-			
-			function completeItem(item: string) {
-				const itemToComplete = new vscode.CompletionItem(item);
-				itemToComplete.kind = vscode.CompletionItemKind.Keyword;
-				return itemToComplete;
-			}
-			
 			return keywords.map((e) => completeItem(e.name));
 		}
 	});
