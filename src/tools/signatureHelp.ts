18d6e69c9f1286c570c018bd59a52adb44fe2eef
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-06-20 10:02:20 +0200

    Updated signature helper

--- src/tools/signatureHelp.ts
+++ src/tools/signatureHelp.ts
@@ -3,34 +3,31 @@ import * as vscode from 'vscode';
 import { Keyword, keywords } from '../constants';
 
 function countCar(s: string, c: string) : number {
-	let cnt : number = 0;
-	const n : number = s.length;
-	let i : number = 0;
+	let cnt = 0;
+	const n = s.length;
+	let i = 0;
 	while (i < n) {
-		if (s[i] === c) {
-			cnt++;
-		}
+		cnt += ((s[i] === c) ? 1 : 0)
 		i++;
 	}
 	return cnt; 
 }
 
-// todo: generalize (code duplication...)
 function triggerSignatureHelp(keyword: Keyword) {
 	return vscode.languages.registerSignatureHelpProvider('tchecker', {
 		provideSignatureHelp(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.SignatureHelpContext) {
 
 			const linePrefix = document.lineAt(position).text.substring(0, position.character);
-			//console.log(countCar(linePrefix,':'));
 
 			let nbOfColon = countCar(linePrefix,':');
+			let nbOfOpenBracket = countCar(linePrefix,'{');
 
-			if (!linePrefix.startsWith(keyword.name + ':') || (nbOfColon > keyword.attributePos)) {
+			if (!linePrefix.startsWith(keyword.name + ':') || (nbOfColon > keyword.attributePos) || (nbOfOpenBracket > 1) || (nbOfOpenBracket == 1 && nbOfColon < keyword.attributePos)) {
 				return undefined;
 			}
 
 			return {
-				activeParameter: nbOfColon - 1,
+				activeParameter: (nbOfOpenBracket == 1) ? keyword.attributePos : (nbOfColon - 1),
 				activeSignature: 0,
 				signatures: [{
 					label: keyword.signature,
@@ -40,34 +37,9 @@ function triggerSignatureHelp(keyword: Keyword) {
 			};
 		}
 	},
-	':');
+	':','{');
 }
-
-function triggerAttributeSignatureHelp(keyword: Keyword) {
-	return vscode.languages.registerSignatureHelpProvider('tchecker', {
-		provideSignatureHelp(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.SignatureHelpContext) {
-			
-			const linePrefix = document.lineAt(position).text.substring(0, position.character);
-			let nbOfColon = countCar(linePrefix,':');
-			if ((!linePrefix.startsWith(keyword.name + ':')) || (!linePrefix.endsWith('{')) || (nbOfColon != keyword.attributePos)) {
-				return undefined;
-			}
-
-			return {
-				activeParameter: keyword.attributePos,
-				activeSignature: 0,
-				signatures: [{
-					label: keyword.signature,
-					documentation: keyword.documentation,
-					parameters: keyword.parameters
-				}]
-			};
-		}
-	},
-	'{');
-}
-
 
 export function systemSignatureHelp() {
-	return keywords.map((e) => triggerSignatureHelp(e)).concat(keywords.map((e) => triggerAttributeSignatureHelp(e)));
+	return keywords.map((e) => triggerSignatureHelp(e));
 }
