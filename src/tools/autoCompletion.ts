9953f60da061c668744fc20b29f32c25f9255ecf
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-06-20 10:40:44 +0200

    Refactored auto completion

--- src/tools/autoCompletion.ts
+++ src/tools/autoCompletion.ts
@@ -15,27 +15,3 @@ export function keywordsCompletion() {
 		}
 	});
 }
-
-// testing features (to come: auto-providing events...)
-// let currentEvents = [
-// 	'x',
-// 	'y'
-// ];
-
-// // todo: refactoring this code... (this is only a demo)
-// export function eventNext() {
-// 	return vscode.languages.registerCompletionItemProvider('tchecker', {
-
-// 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-			
-// 			const linePrefix = document.lineAt(position).text.substring(0, position.character);
-// 			if (!linePrefix.endsWith('event:')) {
-// 				return undefined;
-// 			}
-
-// 			return currentEvents.map((e) => new vscode.CompletionItem(e, vscode.CompletionItemKind.Variable));
-// 		}
-// 	},
-// 	':'
-// 	);
-// }
