6e5fadfec532115a1277ef74d35c175afa6c9928
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-07-17 11:22:30 +0200

    Updated countCar

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,7 @@
 export function countCar(s: string, c: string) : number {
-	let cnt = 0;
-	const n = s.length;
-	let i = 0;
-	while (i < n) {
-		cnt += ((s[i] === c) ? 1 : 0)
-		i++;
+	let cnt = -1;
+	for (let idx = 0; idx !== -1; idx=s.indexOf(c,idx+1)) {
+		cnt++;
 	}
-	return cnt; 
+	return cnt;
 }
