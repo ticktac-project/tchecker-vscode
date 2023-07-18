830bd763ad3ad75c75af775ba879f0bbc5ccb1d4
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-07-18 13:02:50 +0200

    Documented utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,18 @@
+/**
+ * This module provides utility functions.
+ */
+
 import * as vscode from 'vscode';
 import { getCurrentFileErrorMessage } from './constants';
 
+
+/**
+ * Counts the number of occurrences of 'c' in s.
+ * 
+ * @param s A string
+ * @param c The targeted character
+ * @returns The number of occurrences of 'c' in s
+ */
 export function countChar(s: string, c: string) : number {
 	let cnt = -1;
 	for (let idx = 0; idx !== -1; idx=s.indexOf(c,idx+1)) {
@@ -9,6 +21,13 @@ export function countChar(s: string, c: string) : number {
 	return cnt;
 }
 
+
+/**
+ * Gets the current (or latest) editing file.
+ * 
+ * @param currentEditor The current (or latest) VSC editor
+ * @returns The current (or latest) editing file
+ */
 export function getCurrentFile(currentEditor: vscode.TextEditor) {
 	const currentFile = currentEditor.document.uri.fsPath;
 	if (currentFile === '') {
