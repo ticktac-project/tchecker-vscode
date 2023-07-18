b435e6508c1c23f15b2c0d1544b237055cdbf53d
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-07-18 13:25:36 +0200

    Documented extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,7 @@
+/**
+ * This module initializes the extension.
+ */
+
 import * as vscode from 'vscode';
 
 import { handleTckSyntax } from './tools/tckSyntax'
@@ -8,12 +12,20 @@ import { systemSignatureHelp } from './tools/signatureHelp';
 import { handleHover } from './tools/hover';
 
 export let currentEditor : vscode.TextEditor = vscode.window.activeTextEditor as vscode.TextEditor;
+
+// watch editor changes
 vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined) => {
 	if (editor && editor.document.uri.scheme === 'file') {
 		currentEditor = editor;
 	}
 });
 
+
+/**
+ * Activates the extension.
+ * 
+ * @param context VSC context
+ */
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Extension activated.');
 	const diagnosticCollection = vscode.languages.createDiagnosticCollection('errors');
