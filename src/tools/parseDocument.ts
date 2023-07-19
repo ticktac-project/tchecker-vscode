fdc11de403b5280b0b2dc983aee09a17995baedf
Guilhem ARDOUIN <gardouin@enseirb.fr>
2023-07-19 10:44:22 +0200

    Updated parseDocument

--- src/tools/parseDocument.ts
+++ src/tools/parseDocument.ts
@@ -1,14 +1,36 @@
+/**
+ * This module provides tools for document parsing.
+ */
+
 import { SpawnSyncReturns } from 'child_process';
+
 import * as vscode from 'vscode';
 
+
+/**
+ * Gets all variables associated to a TChecker keyword above the user position
+ * in the text document.
+ * 
+ * In order to target a specific variable, we specify the associated keyword,
+ * and the position of the variable in the declaration.
+ * 
+ * See: https://github.com/ticktac-project/tchecker/wiki/TChecker-file-format for
+ * more information about the expected index of the different variables.
+ * 
+ * @param document VSC document
+ * @param keyword TChecker's keyword
+ * @param index The position of the variable in the declaration (0 is the keyword,
+ *              1 is the next variable...)
+ * @param constraint Additional constraint (can be left blank, i.e. '')
+ * @returns An array of targeted TChecker's variables
+ */
 export function getVarAbove(document: vscode.TextDocument, keyword: string, index: number, constraint: string) {
 	let pos : vscode.Position;
 	let line;
-	let i = 0;
 	// in order to get defined variables, we consider the current line as the last line for the document analysis
 	const lastLine : number =  vscode.window.activeTextEditor?.selection.active.line as number;
 	const res : string[] = [];
-	while (i < lastLine) {
+	for (let i = 0; i < lastLine; i++) {
 		pos = new vscode.Position(i, 0);
 		line = document.lineAt(pos).text;
 		if (line.startsWith(keyword + '')) {
@@ -22,18 +44,24 @@ export function getVarAbove(document: vscode.TextDocument, keyword: string, inde
 				res.push(value);
 			}
 		}
-		i++;
 	}
 	return res;
 }
 
+
+/**
+ * Parses error and warning messages from TChecker's output.
+ * 
+ * @param output Child process output
+ * @param severity Error severity (either error or warning)
+ * @returns An array of VSC diagnostics
+ */
 export function parseErrorPosition(output: SpawnSyncReturns<string>, severity: vscode.DiagnosticSeverity) {
 	const regex = /[0-9]+-*[0-9]*\b.\b[0-9]+-*[0-9]*/;
 	const stderr = output.stderr.split('\n');
 	const errors = [];
-	let i = 0;
 
-	while (i < stderr.length - 1) {
+	for (let i = 0; i < stderr.length - 1; i++) {
 		const errorPosition = stderr[i].match(regex);
 		if (errorPosition !== null) {
 			if (errorPosition[0] !== '') {
@@ -48,11 +76,10 @@ export function parseErrorPosition(output: SpawnSyncReturns<string>, severity: v
 				errors.push(new vscode.Diagnostic(range, stderr[i], severity));
 			}
 		} else {
-			const pos = new vscode.Position(0, 0);
+			const pos = new vscode.Position(0, 0); // default error position if not found
 			const range = new vscode.Range(pos, pos);
 			errors.push(new vscode.Diagnostic(range, stderr[i], severity));
 		}
-		i++;
 	}
 
 	return errors;
